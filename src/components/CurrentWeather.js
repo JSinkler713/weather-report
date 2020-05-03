52a37155a1d1f616e528483ed32436f71abd2e92
James Sinkler <jamessinkler713@gmail.com>
2020-05-03 16:46:12 -0700

    adds more details on current weather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,13 +4,20 @@ import '../main.css'
 class CurrentWeather extends Component {
   render(){  
    let tempHigh = (((this.props.weather.main.temp_max - 273.15) * 1.8) + 32).toFixed(1)
+   let tempLow = (((this.props.weather.main.temp_min - 273.15) * 1.8) + 32).toFixed(1)
+   let tempNow = (((this.props.weather.main.temp - 273.15) * 1.8) + 32).toFixed(1)
+   let windGust = (this.props.weather.wind.gust * 2.23).toFixed(1);
+   let windSpeed = (this.props.weather.wind.speed * 2.23).toFixed(1);
 
     return(
       <div> 
         <ul>
           <li>Place: {this.props.weather.name}</li>
           <li>Description: {this.props.weather.weather[0].description}</li>
+          <li>Currently {tempNow} Degrees</li>
+          <li>Low: {tempLow} Degrees</li>
           <li>High: {tempHigh} Degrees</li>
+          <li>Wind gusts up to {windGust}mph with avg of {windSpeed}mph</li> 
         </ul>
       </div>
     )
