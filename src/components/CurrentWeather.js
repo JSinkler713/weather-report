ea2fc2432c680d837eb1dd2f9523f210ba23436a
James Sinkler <jamessinkler713@gmail.com>
2020-05-06 08:36:45 -0700

    edits what data is displayed

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -3,8 +3,6 @@ import '../main.css'
 
 class CurrentWeather extends Component {
   render(){  
-   let tempHigh = (((this.props.weather.main.temp_max - 273.15) * 1.8) + 32).toFixed(1)
-   let tempLow = (((this.props.weather.main.temp_min - 273.15) * 1.8) + 32).toFixed(1)
    let tempNow = (((this.props.weather.main.temp - 273.15) * 1.8) + 32).toFixed(1)
    let windGust = (this.props.weather.wind.gust * 2.23).toFixed(1);
    let windSpeed = (this.props.weather.wind.speed * 2.23).toFixed(1);
@@ -26,8 +24,6 @@ class CurrentWeather extends Component {
           <li>Place: {this.props.weather.name}</li>
           <li>Description: {this.props.weather.weather[0].description}</li>
           <li>Currently {tempNow} Degrees</li>
-          <li>Low: {tempLow} Degrees</li>
-          <li>High: {tempHigh} Degrees</li>
           <li>Wind gusts up to {windGust}mph with avg of {windSpeed}mph</li> 
         </ul>
       </div>
