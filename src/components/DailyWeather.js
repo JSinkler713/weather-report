283cbc7e5dd0e4dfd141fe3173570c4383854fed
James Sinkler <jamessinkler713@gmail.com>
2020-05-13 22:15:54 -0700

    uses hash map to get most frequent weather description

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -8,9 +8,50 @@ class DailyWeather extends Component {
     // TODO currently first appearing icon, change to most frequent
     // let iconCode = this.props.weather.weather[0].icon
     // let iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`
+    
+
     // Also need to get most occuring description
+    // And most occuring icon
+    let description;
+    let icon;
+    let highestCount = 0;
+    let highestIconCount = 0;
+    let descriptions = {}
+    let icons = {}
+    let frequentIcon;
+    let frequentDescription;
+    
+    for (let i=0; i < 8; i++) {
+      description = this.props.weather[i].weather[0].description;
+      icon = this.props.weather[i].weather[0].icon
+      // get icon hash map      
+      if (icons[icon] === undefined) {
+        icons[icon] = 1;
+      } else {
+        icons[icon] = icons[icon] + 1
+      }
+      if (icons[icon] > highestIconCount) {
+        highestIconCount = icons[icon];
+        frequentIcon = icon;
+      }
 
-
+      // description hash map
+      if (descriptions[description] === undefined) {
+        descriptions[description] = 1;
+      } else {
+        descriptions[description] = descriptions[description] + 1
+      }
+      // check if highest count
+      if (descriptions[description] > highestCount) {
+        highestCount = descriptions[description];
+        frequentDescription = description;
+      }
+    }
+    frequentIcon = frequentIcon.slice(0,2) + 'd'
+     
+    console.log(frequentDescription);
+    
+    // get the high and low temperatures
     let temps = this.props.weather.map((period)=> {
       return period.main.temp
     })
@@ -23,17 +64,16 @@ class DailyWeather extends Component {
     temps.forEach(temp=> sum+=temp)
     let avg = sum / 8;
     avg = (((avg - 273.15) * 1.8) + 32).toFixed(1)
-    console.log(`the high is: ${high}`)
-    console.log(`the low is: ${low}`)
-    console.log(`the avg is ${avg}`)
     let date = this.props.weather[0].dt_txt.split(" ")
     return(
       <div className='DailyWeather'> 
         <ul>
+          <li>Icon: {frequentIcon}</li>
           <li>Date: {date[0]}</li>
           <li>High: {high}</li>
           <li>Low: {low}</li>
           <li>Average Temp: {avg}</li>
+          <li>Description: {frequentDescription}</li>
         </ul>
       </div>
     )
