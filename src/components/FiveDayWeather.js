9051aa47e299ea309879d24f5eb39fc0d3617850
James Sinkler <jamessinkler713@gmail.com>
2020-05-16 12:11:38 -0700

    refactors to functional component

--- src/components/FiveDayWeather.js
+++ src/components/FiveDayWeather.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import DailyWeather from './DailyWeather';
 import '../main.css' 
 
-class FiveDayWeather extends Component {
-  render(){  
-    return(
-      <div className='FiveDayWeather' > 
-        <DailyWeather weather={this.props.list.slice(0,8)} />
-        <DailyWeather weather={this.props.list.slice(8, 16)} />
-        <DailyWeather weather={this.props.list.slice(16, 24)} />
-        <DailyWeather weather={this.props.list.slice(24, 32)} />
-        <DailyWeather weather={this.props.list.slice(32, 40)} />
-      </div>
-    
-    )
-  }
+const FiveDayWeather = ({ list})=> {
+  return(
+    <div className='FiveDayWeather' > 
+      <DailyWeather weather={list.slice(0,8)} />
+      <DailyWeather weather={list.slice(8, 16)} />
+      <DailyWeather weather={list.slice(16, 24)} />
+      <DailyWeather weather={list.slice(24, 32)} />
+      <DailyWeather weather={list.slice(32, 40)} />
+    </div>
+  )
 }
 
 export default FiveDayWeather;
